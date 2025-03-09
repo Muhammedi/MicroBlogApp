5c1354649944a78201c4cd51727e655ecde48b11
Muhammed Hamed <Muhammed.Hamed@geidea.net>
2025-03-10 00:04:39 +0200

    clean

--- Model/Request/Post.cs
+++ Model/Request/Post.cs
@@ -10,7 +10,7 @@ namespace Model.Request
     {
         public int Id { get; set; }
         public string Text { get; set; }
-        public string ImageUrl { get; set; }
+        public string ImageUr { get; set; }
         public DateTime CreatedAt { get; set; }
         public double Latitude { get; set; }
         public double Longitude { get; set; }
